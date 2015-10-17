6413f2d3ff5f7c75b1d8099ed9c86a2268239893
diego <diego@192.168.0.13>
2015-10-17 11:24:33 +0200

    configurado la seguridad.

--- memosDDR/src/main/config/AppConfig.java
+++ memosDDR/src/main/config/AppConfig.java
@@ -10,10 +10,12 @@ package main.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration 
-@ComponentScan("/") 
+@ComponentScan({"main.*"}) 
 @EnableWebMvc 
+@Import({ SecurityConfiguration.class })
 public class AppConfig {
 
 }
--- memosDDR/src/main/config/WebAppInitializer.java
+++ memosDDR/src/main/config/WebAppInitializer.java
@@ -3,18 +3,31 @@
  */
 package main.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.Dynamic;
-
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
  * @author ddiaz
  *
  */
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	 
+@Override
+protected Class<?>[] getRootConfigClasses() {
+return new Class[] { AppConfig.class };
+}
+
+@Override
+protected Class<?>[] getServletConfigClasses() {
+return null;
+}
+
+@Override
+protected String[] getServletMappings() {
+return new String[] { "/" };
+}
+
+}
+/*
 public class WebAppInitializer implements WebApplicationInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {  
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
@@ -26,3 +39,4 @@ public class WebAppInitializer implements WebApplicationInitializer {
 	}
 
 }
+*/
